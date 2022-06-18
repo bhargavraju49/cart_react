6690f1463efb869b417cb07d4b7d8d63f8b6c86a
bhargav penumetsa <bobby.srrp@gmail.com>
2022-06-18 18:38:57 +0530

    firebase add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navbar from "./Navbar";
 import Sample from "./Sample";
 
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, onSnapshot, DocumentReference, addDoc  } from 'firebase/firestore/lite';
+// import { onSnapshot } from 'firebase/firestore';
+
 import firebaseApp from "./index";
 
 class App extends React.Component {
@@ -14,27 +16,45 @@ class App extends React.Component {
       products: [],
       loading: true
     }
+    this.db = getFirestore(firebaseApp);
   }
   componentDidMount(){
 
     const db = getFirestore(firebaseApp);
-    // Get a list of cities from your database
+    // Get a list of products from your database
+    // const getproducts = async (db) => {
+    //   const productCols = collection(db, 'Products');
+    //   const productSnap = await getDocs(productCols);
+    //   const productList = productSnap.docs.map((doc) => {
+    //     const data = doc.data();
+    //     data['id'] = doc.id
+    //     return data;
+    //   });
+    //   console.log(productList)
+    //   this.setState({
+    //     products:productList,
+    //     loading:false
+    //   })
+    // }
+    // getproducts(db);
+
+
     const getproducts = async (db) => {
       const productCols = collection(db, 'Products');
-      const productSnap = await getDocs(productCols);
-      const productList = productSnap.docs.map((doc) => {
-        const data = doc.data();
-        data['id'] = doc.id
-        return data;
+      const productSnap = await onSnapshot(productCols,(products)=>{
+        const productList = products.docs.map((doc) => {
+          const data = doc.data();
+          data['id'] = doc.id
+          return data;
+        });
+        console.log(productList)
+        this.setState({
+          products:productList,
+          loading:false
+        })
       });
-      console.log(productList)
-      this.setState({
-        products:productList,
-        loading:false
-      })
     }
     getproducts(db);
-
   }
 
     handleIncrementQuantity = (product) => {
@@ -88,21 +108,33 @@ class App extends React.Component {
     const {products} = this.state
     let count = 0;
     products.forEach((product)=>{
-        count+=product.price
+        count+=product.price*product.qty
     })
     return count
+}
+   addProduct = async ()=>{
+    const docRef = await addDoc(collection(this.db, "Products"), {
+      img:'',
+      price:30000,
+      qty:1,
+      title:'washing machine'
+    });
+    console.log("Document written with ID: ", docRef.id);
+    
 }
   render(){
     const {products , loading} = this.state
     return (
       <div className="App">
         <Navbar count = {this.getCartCount()}></Navbar>
+        <button onClick={this.addProduct}>add a product</button>
         <Cart
         products = {products}
          onIncreaseQuantity = {this.handleIncrementQuantity} 
          onDecreaseQuantity = {this.handleDecrementQuantity}
          onDelete = {this.handleDelete}
          > </Cart>
+
          {loading && <h1>loading...</h1>}
          <div style={{padding:30}}><h1>Total = {this.getTotal()}</h1></div>
          
